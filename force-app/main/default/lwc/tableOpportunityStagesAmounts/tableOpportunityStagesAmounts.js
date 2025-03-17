a2f81097cb98f84b255347bf436557e74355e62f
Володя Висоцький <74934018+Blackdeer2@users.noreply.github.com>
2025-03-17 12:57:10 +0200

    add Vertical table

--- force-app/main/default/lwc/tableOpportunityStagesAmounts/tableOpportunityStagesAmounts.js
+++ force-app/main/default/lwc/tableOpportunityStagesAmounts/tableOpportunityStagesAmounts.js
@@ -10,34 +10,70 @@ export default class TableOpportunityStagesAmounts extends LightningElement {
     @api recordId;
     @track columns = [];
     @track tableData = [];
-    isVertical = false;
+    @track data;
+    isVertical = true;
 
     @wire(getStagesWithTotalAmount, { accountId: '$recordId' })
     wiredStages({ error, data }) {
         if (data) {
             console.log('Raw Data from Apex:', JSON.stringify(data));
-
-            this.columns = ALL_STAGES.map(stage => ({
-                label: stage,
-                fieldName: stage,
-                type: 'text'
-            }));
-
-            const rowDataTotal = { id: 'totalRow', label: 'Total Amount' };
-            const rowDataOpportunities = { id: 'opportunitiesRow', label: 'Opportunities' };
-
-            const amounts = data.amounts;
-            const opportunities = data.opportunities;
-
-            ALL_STAGES.forEach(stage => {
-                rowDataTotal[stage] = amounts[stage];
-                rowDataOpportunities[stage] = opportunities[stage] ? opportunities[stage].join('\n') : '-';
-            });
-
-            this.tableData = [rowDataTotal, rowDataOpportunities];
+            this.data = data;
+            this.updateTable(data);
 
         } else if (error) {
             console.error('Error fetching stages with total amount:', error);
         }
+    }
+
+    updateTable(data){
+        if(this.isVertical){
+            this.generateVerticalTable(data);
+        }else{
+            this.generateHorizontalTable(data);
+        }
+    }
+
+    generateHorizontalTable(data){
+
+        this.columns = ALL_STAGES.map(stage => ({
+            label: stage,
+            fieldName: stage,
+            type: 'text'
+        }));
+
+        const rowDataTotal = { id: 'totalRow', label: 'Total Amount' };
+        const rowDataOpportunities = { id: 'opportunitiesRow', label: 'Opportunities' };
+
+        const amounts = data.amounts;
+        const opportunities = data.opportunities;
+
+        ALL_STAGES.forEach(stage => {
+            rowDataTotal[stage] = amounts[stage];
+            rowDataOpportunities[stage] = opportunities[stage] ? opportunities[stage].join('\n') : '-';
+        });
+
+        this.tableData = [rowDataTotal, rowDataOpportunities];
+    }
+
+    
+    generateVerticalTable(data){
+
+        this.columns = [
+            { label: 'Stage', fieldName: 'stage', type: 'text' },
+            { label: 'Total Amount', fieldName: 'totalAmount', type: 'currency' },
+            { label: 'Opportunities', fieldName: 'opportunities', type: 'text' }
+        ];
+
+        this.tableData = ALL_STAGES.map(stage => ({
+            id: stage,
+            stage: stage,
+            totalAmount: data.amounts[stage] || 0,
+            opportunities: data.opportunities[stage] ? data.opportunities[stage].join(', ') : '-'
+        }));
+    }
+
+    handleSwitch(){
+        this.isVertical =!this.isVertical;
+        this.updateTable(this.data);
     }
 }
